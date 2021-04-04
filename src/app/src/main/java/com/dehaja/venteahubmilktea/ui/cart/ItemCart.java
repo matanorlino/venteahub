9e447545220729c89b75d310e96cac1f54b2b67e
matanorlino <matanorlino122@gmail.com>
2021-04-04 12:17:00 +0800

    Added Item Cart activity

---
+++ src/app/src/main/java/com/dehaja/venteahubmilktea/ui/cart/ItemCart.java
@@ -0,0 +1,16 @@
+package com.dehaja.venteahubmilktea.ui.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dehaja.venteahubmilktea.R;
+
+public class ItemCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_cart);
+    }
+}
